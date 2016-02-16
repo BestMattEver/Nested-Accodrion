4732e1b5db88886e7252a1aed6b67127c472d319
BestMatEver <mattkun@gmail.com>
2016-02-16 18:07:44 -0500

    did hard more and nightmare mode. procedurally

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -3,12 +3,22 @@ console.log(headings);
 
 $(".clickme").click(function(e){
   e.preventDefault();
-  //console.log($(this).children().first().next());//this prints out the article element im looking for.
-  //$(this).children().first().next().toggle().addClass("visible");//this toggles on and off the visible class on that article element
   $(this).siblings("#article").toggle().addClass("visible");
-  $(this).find("#icon").addClass(".down");
 
-
+//this closes all the article elements EXCEPT the one i clicked on.
+  for(var i=0;i<headings.length;i++)
+  {
+    if(headings[i] === this)
+    {
+      console.log(true);
+    }
+    else
+    {
+      $(headings[i]).siblings("#article").css("display","none");
+      // $(headings[i]).siblings("#article").removeClass("visible");
+      console.log(false);
+    }
+  }
   /*
   console.log($(this).children().first().children().first().find("#icon"));//this line navigates through the hierarchy to get the thing i want
   console.log($(this).find("#icon"));//this one searches through all descendents and finds the instance of #icon among them.
@@ -20,8 +30,5 @@ $(".nestling-click").click(function(e){
   e.preventDefault();
   $(this).siblings(".nestlingContent").toggle().addClass("visible");//this SHOULD show the nestling content.
   console.log($(this).siblings(".nestlingContent"));
-
-
-  $(this).find(".nestlingArrow").toggle().addClass("down");//this makes the arrow work
-});
-// $("#closeModal").click(hideModal);
+  //$(this).find(".nestlingArrow").toggle().addClass("down");//this makes the arrow work
+});//end nestling click
